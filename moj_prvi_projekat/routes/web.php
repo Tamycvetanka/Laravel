c85f1d60e1f8c8755f5d67f955912950c6cd61a1
Tamycvetanka <cvetkovskatamara2@gmail.com>
2025-07-28 10:15:37 +0200

    test

--- moj_prvi_projekat/routes/web.php
+++ moj_prvi_projekat/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/about', 'about');
+Route::view('/shop', 'shop');
+Route::view('/contact', 'contact');
+
